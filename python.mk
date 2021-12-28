323d679e4eafc5b72a63b8c8e0e301a3d0a76e81
Eugene Scherba <escherba@gmail.com>
2021-12-27 22:16:52 -0800

    switch back to legacy PyPI api endpoint

--- python.mk
+++ python.mk
@@ -3,7 +3,7 @@ EXTENSION := $(PYMODULE).so
 SRC_DIR := src
 EXTENSION_INTERMEDIATE := ./$(SRC_DIR)/$(PYMODULE).cpp
 EXTENSION_DEPS := ./$(SRC_DIR)/$(PYMODULE).pyx
-PYPI_URL := https://pypi.python.org/pypi/
+PYPI_URL := https://upload.pypi.org/legacy/
 UNAME_S := $(shell uname -s)
 DISTRIBUTE := sdist
 ifeq ($(UNAME_S),Darwin)
