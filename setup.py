f5cf22afec3913694c63ec60785eaaf86141a4ad
Eugene Scherba <escherba@gmail.com>
2021-12-31 20:33:51 -0800

    black-format cpuinfo and setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,14 @@ from setuptools.extension import Extension
 
 try:
     from cpuinfo import get_cpu_info
-    CPU_FLAGS = get_cpu_info()['flags']
+
+    CPU_FLAGS = get_cpu_info()["flags"]
 except Exception as exc:
     CPU_FLAGS = {}
 
 try:
     from Cython.Distutils import build_ext
+
     USE_CYTHON = True
 except ImportError:
     USE_CYTHON = False
@@ -25,6 +27,7 @@ class BinaryDistribution(Distribution):
     Subclass the setuptools Distribution to flip the purity flag to false.
     See https://lucumr.pocoo.org/2014/1/27/python-on-wheels/
     """
+
     def is_pure(self):
         """Returns purity flag"""
         return False
@@ -38,25 +41,27 @@ print("available CPU flags: %s" % CPU_FLAGS)
 if os.name == "nt":
     CXXFLAGS.extend(["/O2"])
 else:
-    CXXFLAGS.extend([
-        "-O3",
-        "-Wno-unused-value",
-        "-Wno-unused-function",
-    ])
+    CXXFLAGS.extend(
+        [
+            "-O3",
+            "-Wno-unused-value",
+            "-Wno-unused-function",
+        ]
+    )
 
 
 # Note: Only -msse4.2 has significant effect on performance;
 # so not using other flags such as -maes and -mavx
-if 'sse4_2' in CPU_FLAGS:
+if "sse4_2" in CPU_FLAGS:
     print("Compiling with SSE4.2 enabled")
-    CXXFLAGS.append('-msse4.2')
+    CXXFLAGS.append("-msse4.2")
 else:
     print("compiling without SSE4.2 support")
 
 
 if USE_CYTHON:
     print("building extension using Cython")
-    CMDCLASS = {'build_ext': build_ext}
+    CMDCLASS = {"build_ext": build_ext}
     SRC_EXT = ".pyx"
 else:
     print("building extension w/o Cython")
@@ -71,7 +76,7 @@ EXT_MODULES = [
         depends=["src/city.h"],
         language="c++",
         extra_compile_args=CXXFLAGS,
-        include_dirs=['src'],
+        include_dirs=["src"],
     ),
     Extension(
         "farmhash",
@@ -79,11 +84,11 @@ EXT_MODULES = [
         depends=["src/farm.h"],
         language="c++",
         extra_compile_args=CXXFLAGS,
-        include_dirs=['src'],
-    )
+        include_dirs=["src"],
+    ),
 ]
 
-if 'sse4_2' in CPU_FLAGS:
+if "sse4_2" in CPU_FLAGS:
     EXT_MODULES.append(
         Extension(
             "cityhashcrc",
@@ -94,12 +99,12 @@ if 'sse4_2' in CPU_FLAGS:
             ],
             language="c++",
             extra_compile_args=CXXFLAGS,
-            include_dirs=['src'],
+            include_dirs=["src"],
         )
     )
 
 
-VERSION = '0.3.0.post7'
+VERSION = "0.3.0.post7"
 URL = "https://github.com/escherba/python-cityhash"
 
 
@@ -109,10 +114,10 @@ LONG_DESCRIPTION = """
 
 
 def get_long_description():
-    fname = join(dirname(__file__), 'README.rst')
+    fname = join(dirname(__file__), "README.rst")
     try:
-        with open(fname, 'rb') as fh:
-            return fh.read().decode('utf-8')
+        with open(fname, "rb") as fh:
+            return fh.read().decode("utf-8")
     except Exception:
         return LONG_DESCRIPTION
 
@@ -126,34 +131,33 @@ setup(
     maintainer_email="escherba+cityhash@gmail.com",
     url=URL,
     download_url=URL + "/tarball/master/" + VERSION,
-    name='cityhash',
-    license='MIT',
+    name="cityhash",
+    license="MIT",
     zip_safe=False,
     cmdclass=CMDCLASS,
     ext_modules=EXT_MODULES,
-    keywords=['google', 'hash', 'hashing', 'cityhash', 'farmhash', 'murmurhash'],
+    keywords=["google", "hash", "hashing", "cityhash", "farmhash", "murmurhash"],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: C++',
-        'Programming Language :: Cython',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: System :: Distributed Computing',
-        'Topic :: Utilities'
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: C++",
+        "Programming Language :: Cython",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Distributed Computing",
     ],
     long_description=get_long_description(),
-    long_description_content_type='text/x-rst',
-    tests_require=['pytest'],
+    long_description_content_type="text/x-rst",
+    tests_require=["pytest"],
     distclass=BinaryDistribution,
 )
