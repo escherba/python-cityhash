74c98246a76ea1acc9fe283b16aedd66a39930a7
Eugene Scherba <escherba@gmail.com>
2022-11-24 22:58:23 -0800

    v0.4.4 (#77)

--- src/cityhashcrc.pyx
+++ src/cityhashcrc.pyx
@@ -10,7 +10,7 @@ Python wrapper for CityHash-CRC
 
 __author__      = "Eugene Scherba"
 __email__       = "escherba+cityhash@gmail.com"
-__version__     = '0.4.3'
+__version__     = '0.4.4'
 __all__         = [
     "CityHashCrc128",
     "CityHashCrc128WithSeed",
